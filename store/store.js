49120469560d550a763017e49dcbf63679c65732
mkcmgl <2515318247@qq.com>
2022-11-08 10:50:43 +0800

    tabbar-store

--- store/store.js
+++ store/store.js
@@ -3,6 +3,7 @@ import {observable,action} from 'mobx-miniprogram'
 export const store=observable({
  A:1,
  B:2,
+ activeTabBarIndex:0,
  get sum(){
    return this.A+this.B
  },
@@ -12,4 +13,7 @@ export const store=observable({
  updateB:action(function(val){
   this.B+=val
 }),
+updateActiveTabBarIndex:action(function(val){
+  this.activeTabBarIndex=val
+})
 })
