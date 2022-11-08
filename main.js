bb3459caf99dc284153af41c0ea245123a015ee2
mkcmgl <2515318247@qq.com>
2022-11-08 17:20:35 +0800

    uni-swiper

--- main.js
+++ main.js
@@ -2,6 +2,22 @@
 // #ifndef VUE3
 import Vue from 'vue'
 import App from './App'
+import {$http} from "@escook/request-miniprogram"	
+
+uni.$http=$http
+
+$http.baseUrl='https://applet-base-api-t.itheima.net'
+// 轮播图的接口：https://applet-base-api-t.itheima.net/slides
+// 九宫格接口：https://applet-base-api-t.itheima.net/categories
+// 本地生活 获取并渲染商铺列表的数据：https://applet-base-api-t.itheima.net/categories/${this.data.query.id}/shops
+$http.beforeRequest=function(options){
+	uni.showLoading({
+		title:'加载中。。'
+	})
+}
+$http.afterRequest=function(options){
+	uni.hideLoading()
+}
 
 Vue.config.productionTip = false
 
--- pages/home/home.vue
+++ pages/home/home.vue
@@ -1,6 +1,16 @@
 <template>
 	<view>
-		home
+		<!-- 轮播图 -->
+		<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="1000" :circular="true">
+			<swiper-item  v-for="(item,index) in imgList" :key="item.id">
+				<view class="swiper-item">
+					<img :src="item.image" >
+				</view>
+			</swiper-item>
+
+		</swiper>
+		
+		
 	</view>
 </template>
 
@@ -8,12 +18,39 @@
 	export default {
 		data() {
 			return {
-				
+				imgList:{}
 			};
+		},
+		onLoad() {
+			this.gitSwiperList()
+		},
+		methods:{
+		async gitSwiperList(){
+			console.log('????????????')
+			const res=await	uni.$http.get('/slides')
+			console.log(res)
+			if(res.statusCode !==200){
+				return uni.showToast({
+					title:'数据请求失败',
+					duration:1500,
+					icon:'none'
+				})
+			}
+		
+				this.imgList=res.data
+		
+			}
 		}
 	}
 </script>
 
 <style lang="scss">
+swiper{
+	height:330rpx;
+	.swiper-item,image{
+		width: 100%;
+		height: 100%;
+	}
+}
 
 </style>
