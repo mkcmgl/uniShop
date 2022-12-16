d60559ac5b30e7bc596be561ba522bae268825cc
mkcmgl <2515318247@qq.com>
2022-12-16 14:04:45 +0800

    onPullDownRefresh

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -24,6 +24,7 @@
 				},
 				goodsList:[],
 				total:0,
+				isloding:false
 			};
 		},
 		onLoad(options){
@@ -32,12 +33,32 @@
 			  this.getGoodsList()
 		},
 		methods:{
-			async getGoodsList(){
+			async getGoodsList(cd){
+				this.isloding=true
 			 const{data:res}=	await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+			 cd&&cd()
 			 if(res.meta.status!==200) return uni.$showMsg()
-			   this.goodsList = res.message.goods
+			   this.goodsList = [...this.goodsList,...res.message.goods]
 			     this.total = res.message.total
+				 this.isloding=false
 			}
+		},
+		onReachBottom(){
+			if(this.isloding) return
+			if(this.queryObj.pagenum * this.queryObj.pagesize>=this.total) {
+				console.log('数据加载完毕')
+				return uni.$showMsg('数据加载完毕!')
+			}
+			this.queryObj.pagenum+= 1       
+			this.getGoodsList()
+		},
+		onPullDownRefresh(){
+			this.queryObj.pagenum=1
+			this.isloding=false
+			this.total=0
+			this.goodsList=[]
+			
+			this.getGoodsList(()=>uni.stopPullDownRefresh())
 		}
 	}
 </script>
