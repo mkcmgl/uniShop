a88159eb809766e0842bfc6e69aa958dbac29e98
mkcmgl <2515318247@qq.com>
2022-12-16 11:35:44 +0800

    goodsList

--- subpkg/goods_list/goods_list.vue
+++ subpkg/goods_list/goods_list.vue
@@ -1,6 +1,10 @@
 <template>
 	<view>
-		
+		<view class="goods-list">
+			<block v-for="(item,i) in goodsList" :key="i">
+			<my-goods :goods="item"></my-goods>
+			</block>
+		</view>
 	</view>
 </template>
 
@@ -8,8 +12,32 @@
 	export default {
 		data() {
 			return {
-				
+				queryObj:{
+					      // 查询关键词
+					      query: '',
+					      // 商品分类Id
+					      cid: '',
+					      // 页码值
+					      pagenum: 1,
+					      // 每页显示多少条数据
+					      pagesize: 10
+				},
+				goodsList:[],
+				total:0,
 			};
+		},
+		onLoad(options){
+			  this.queryObj.query = options.query || ''
+			  this.queryObj.cid = options.cid || ''
+			  this.getGoodsList()
+		},
+		methods:{
+			async getGoodsList(){
+			 const{data:res}=	await uni.$http.get('/api/public/v1/goods/search', this.queryObj)
+			 if(res.meta.status!==200) return uni.$showMsg()
+			   this.goodsList = res.message.goods
+			     this.total = res.message.total
+			}
 		}
 	}
 </script>
--- subpkg/search/search.vue
+++ subpkg/search/search.vue
@@ -18,10 +18,10 @@
 		 <view class="history" v-else>
 		 	<view class="history-tittle">
 		 		<text>搜索历史</text>
-				<uni-icons type="trash" size="16"></uni-icons>
+				<uni-icons type="trash" size="16" @click="clear"></uni-icons>
 		 	</view>
 			<view class="history-list">
-				<uni-tag :text="item" v-for="(item,i) in history" :key="i"></uni-tag>
+				<uni-tag :text="item" v-for="(item,i) in historys" :key="i" @click="gotoGoodsList(item)"></uni-tag>
 			</view>
 		 </view>
 	</view>
@@ -35,8 +35,11 @@
 				timer:null,
 				kw:'',
 				searchResults:[],
-				history:['a','app','apple']
+				history:['a']
 			}
+		},
+		onLoad(){
+			this.history=JSON.parse(uni.setStorageSync('kw') || '[]')
 		},
 		methods: {
 			input(e){
@@ -55,6 +58,7 @@
 				if(res.meta.status!==200) return uni.$showMsg()
 				console.log(res)
 				this.searchResults=res.message
+				this.saveSearchHistory()
 				}
 			},
 			gotoDetail(goods_id) {
@@ -62,10 +66,39 @@
 			    // 指定详情页面的 URL 地址，并传递 goods_id 参数
 			    url: '/subpkg/goods_detail/goods_detail?goods_id=' + goods_id
 			  })
+			},
+			saveSearchHistory(){
+				// this.history.push(this.kw)
+				
+				  // 1. 将 Array 数组转化为 Set 对象
+				  const set = new Set(this.historyList)
+				  // 2. 调用 Set 对象的 delete 方法，移除对应的元素
+				  set.delete(this.kw)
+				  // 3. 调用 Set 对象的 add 方法，向 Set 中添加元素
+				  set.add(this.kw)
+				  // 4. 将 Set 对象转化为 Array 数组
+				  this.historyList = Array.from(set)
+				  uni.setStorageSync('kw',JSON.stringify( this.historyList ))
+			},
+			clear(){
+				this.history=[]
+				uni.setStorageSync('kw','[]')
+			},
+			gotoGoodsList(kw){
+				uni.navigateTo({
+					url: '/subpkg/goods_list/goods_list?query=' + kw
+				})
 			}
 			// search(e){
 				
 			// }
+		},
+		computed: {
+		  historys() {
+		    // 注意：由于数组是引用类型，所以不要直接基于原数组调用 reverse 方法，以免修改原数组中元素的顺序
+		    // 而是应该新建一个内存无关的数组，再进行 reverse 反转
+		    return [...this.history].reverse()
+		  }
 		}
 	}
 </script>
